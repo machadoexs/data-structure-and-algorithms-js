de3ab38390a370e771d73df0c3c54121a45a7afa
Vitor Machado <vitormds22@gmail.com>
2023-11-14 19:45:29 -0300

    continue linked list

--- js/linked-list/linked_list.js
+++ js/linked-list/linked_list.js
@@ -23,9 +23,69 @@ export default class LinkedList {
     }
     this.count++
   }
+
+  removeAt(index) {
+    if(this.validIndex(index)) {
+
+      let current = this.head
+
+      if(this.firstIndex(index)) {
+        this.head = this.head.next
+      } else {
+        const previous = this.getElementAt(index - 1)
+        current = previous.next
+        previous.next = current.next
+      }
+      this.count--
+      return current.element
+    }
+    return undefined
+  }
+
+  insert(element, index) {
+    if(this.validIndex(index)) {
+      const node = new Node(element)
+      if(this.firstIndex(index)) {
+        const current = this.head
+        node.next = current
+        this.head = node
+      } else {
+        const previous = this.getElementAt(index - 1)
+        const current = previous.next
+        node.next = current
+        previous.next = node
+      }
+      this.count++
+      return true
+    }
+    return false
+  }
+
+  getElementAt(index) {
+    if(this.validIndex(index)) {
+      let node = this.head
+      for (let i = 1; i <= index; i++) {
+        node = node.next
+      }
+      return node;
+    }
+    return undefined;
+  }
+
+  validIndex(index) {
+    return index >= 0 && index <= this.count
+  }
+
+  firstIndex(index) {
+    return index === 0
+  }
 }
 
 const list = new LinkedList()
 list.push(15)
 list.push(10)
 console.log(list)
+list.removeAt(1)
+console.log(list)
+list.push(22)
+console.log(list.getElementAt(0))

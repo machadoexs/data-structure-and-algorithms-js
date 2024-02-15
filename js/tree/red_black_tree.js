dc6903def852957167b6f3b60b5084088a2cce1b
Vitor Machado <vitormds22@gmail.com>
2024-02-15 19:15:46 -0300

    finish red black tree

--- js/tree/red_black_tree.js
+++ js/tree/red_black_tree.js
@@ -39,4 +39,97 @@ class RedBlackTree extends BinarySearchTree {
       return this.insertNode(node.right, key)
     }
   }
+
+  fixTreeProperties(node) {
+    while (node && node.parent && node.parent.color.isRed() && node.color !== Colors.BLACK) {
+      let parent = node.parent
+      const grandParent = parent.parent
+      if (grandParent && grandParent.left === parent) {
+        const uncle = grandParent.right
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED
+          parent.color = Colors.BLACK
+          uncle.color = Colors.BLACK
+          node = grandParent
+        }
+        else {
+          if (node === parent.right) {
+            this.rotationRR(parent)
+            node = parent
+            parent = node.parent
+          }
+
+          this.rotationLL(grandParent)
+          parent.color = Colors.BLACK
+          grandParent.color = Colors.RED
+          node = parent
+        }
+      }
+      else {
+        const uncle = grandParent.left
+        if (uncle && uncle.color === Colors.RED) {
+          grandParent.color = Colors.RED
+          parent.color = Colors.BLACK
+          uncle.color = Colors.BLACK
+          node = grandParent
+        }
+        else {
+          if (node === parent.left) {
+            this.rotationLL(parent)
+            node = parent
+            parent = node.parent
+          }
+
+          this.rotationRR(grandParent)
+          parent.color = Colors.BLACK
+          grandParent.color = Colors.RED
+          node = parent
+        }
+      }
+    }
+    this.root.color = Colors.BLACK
+  }
+
+  rotationLL(node) {
+    const tmp = node.left
+    node.left = tmp.right
+    if (tmp.right && tmp.right.key) {
+      tmp.right.parent = node
+    }
+    tmp.parent = node.parent
+    if (!node.parent) {
+      this.root = tmp
+    }
+    else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp
+      }
+      else {
+        node.parent.right = tmp
+      }
+    }
+    tmp.right = node
+    node.parent = tmp
+  }
+
+  rotationRR(node) {
+    const tmp = node.right
+    node.right = tmp.left
+    if (tmp.left && tmp.left.key) {
+      tmp.left.parent = node
+    }
+    tmp.parent = node.parent
+    if (!node.parent) {
+      this.root = tmp
+    } else {
+      if (node === node.parent.left) {
+        node.parent.left = tmp
+      }
+      else {
+        node.parent.right = tmp
+      }
+    }
+    tmp.left = node
+    node.parent = tmp
+  }
 }
